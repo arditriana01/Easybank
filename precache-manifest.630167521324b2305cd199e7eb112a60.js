998b8b62142fbc2a562e513a40d3cdbdebb6c443
ardi triana <arditriana01@gmail.com>
2020-06-16 14:35:38 +0800

    Updates

---
+++ precache-manifest.630167521324b2305cd199e7eb112a60.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ba777941ff09b9d820d1c4ee3df998",
+    "url": "/Easybank/index.html"
+  },
+  {
+    "revision": "39b52a500306ac68e35c",
+    "url": "/Easybank/static/css/main.04ce6d5a.chunk.css"
+  },
+  {
+    "revision": "23abd805a705d952373d",
+    "url": "/Easybank/static/js/2.9de88a0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Easybank/static/js/2.9de88a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b52a500306ac68e35c",
+    "url": "/Easybank/static/js/main.9099d5f8.chunk.js"
+  },
+  {
+    "revision": "a98ba0e3b5e2dead43fb",
+    "url": "/Easybank/static/js/runtime-main.9b991d47.js"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/Easybank/static/media/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/Easybank/static/media/image-confetti.1510e4a2.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/Easybank/static/media/image-currency.0f583e2b.jpg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/Easybank/static/media/image-mockups.edc63e85.png"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/Easybank/static/media/image-plane.c619f6cb.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/Easybank/static/media/image-restaurant.b561f8d5.jpg"
+  }
+]);
